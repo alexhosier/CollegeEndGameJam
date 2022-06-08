758d2c94c404c00dc49e8089638c462f0f1ea384
Alex Hosier <alexhos75@gmail.com>
2022-06-08 16:54:00 +0100

    Update GJ_NetworkManager.cs
    - Update webhook
    - Add username to start webhook
    - Add username to stop webhook

--- Assets/Scripts/Networking/GJ_NetworkManager.cs
+++ Assets/Scripts/Networking/GJ_NetworkManager.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using Mirror;
+using UnityEngine.Profiling;
 
 namespace GJ.Networking
 {
     public class GJ_NetworkManager : NetworkManager
     {
+        
+        // Called on first frame
+        public override void Start()
+        {
+            // Use the default implementation
+            base.Start();
+
+            // Start the update coroutine
+            StartCoroutine(SendUpdateWebhook());
+        }
+
         // When the client connects to the server
         public override void OnServerAddPlayer(NetworkConnectionToClient conn)
         {
             // Use the default implementation
             base.OnServerAddPlayer(conn);
-
-            Debug.Log("Client connected to the server");
         }
 
         // When the server starts
@@ -54,11 +64,40 @@ namespace GJ.Networking
         }
 
         #region Webhooks
+        IEnumerator SendUpdateWebhook()
+        {
+            // Wait seconds
+            yield return new WaitForSeconds(30);
+            
+            // Create new user information
+            WWWForm form = new WWWForm();
+            form.AddField("username", "Server Status");
+            form.AddField("content", $"Server **{PlayerPrefs.GetString("Server_Instance")}** / **Clients:** {numPlayers} / **RAM Allocated:** {Profiler.GetTotalAllocatedMemoryLong()/1024/1024}MB");
+            
+            // Create a post request for discord
+            using (UnityWebRequest www = UnityWebRequest.Post("https://discord.com/api/webhooks/984072238745653328/BqutJJ6tYMVIJiTeb_IfOvcbIc_DK9mgv0Tu8JKJG-P5rd0mSVMl3uwso7Y2JREUxQuD", form))
+            {
+                // Send the web request
+                yield return www.SendWebRequest();
+
+                // Check if the request was sent
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    // Log the error
+                    Debug.LogError(www.error);
+                }
+            }
+            
+            // Restart the process
+            StartCoroutine(SendUpdateWebhook());
+        }
+
         IEnumerator SendStartingWebhook()
         {
             // Send an API log to the discord channel to alert of server 
             WWWForm form = new WWWForm();
-            form.AddField("content", $"Server **{PlayerPrefs.GetString("Server_Instance")}** starting / OS: {SystemInfo.operatingSystem}");
+            form.AddField("username", "Server Status");
+            form.AddField("content", $"Server **{PlayerPrefs.GetString("Server_Instance")}** starting / **OS:** {SystemInfo.operatingSystem} / **RAM:** {SystemInfo.systemMemorySize/1000}GB");
 
             // Create a post request for discord
             using (UnityWebRequest www = UnityWebRequest.Post("https://discord.com/api/webhooks/984072238745653328/BqutJJ6tYMVIJiTeb_IfOvcbIc_DK9mgv0Tu8JKJG-P5rd0mSVMl3uwso7Y2JREUxQuD", form))
@@ -79,6 +118,7 @@ namespace GJ.Networking
         {
             // Send an API log to the discord channel to alert of server stopping
             WWWForm form = new WWWForm();
+            form.AddField("username", "Server Status");
             form.AddField("content", $"Server **{PlayerPrefs.GetString("Server_Instance")}** stopping!");
 
             // Create a post request for discord
