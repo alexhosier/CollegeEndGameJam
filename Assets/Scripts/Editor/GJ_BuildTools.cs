16fa158f9f9b01361522c7d8e4bb17e784ca7568
Alex Hosier <alexhos75@gmail.com>
2022-06-08 17:23:52 +0100

    Add editor build tools
    - Add Windows client
    - Add Windows server
    - Add Linux server
    - Add WebGL client

---
+++ Assets/Scripts/Editor/GJ_BuildTools.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+namespace GJ.Editor
+{
+    public class GJ_BuildTools : MonoBehaviour
+    {
+        [MenuItem("Game Jam/Build Tools/Windows/Client")]
+        public static void BuildWindowsClient()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/Client.unity", "Assets/Scenes/Game.unity" };
+            buildPlayerOptions.locationPathName = "Builds/Client/Windows/client.exe";
+            buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+
+        [MenuItem("Game Jam/Build Tools/Windows/Server")]
+        public static void BuildWindowsServer()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/Server.unity", "Assets/Scenes/Game.unity" };
+            buildPlayerOptions.locationPathName = "Builds/Server/Windows/server.exe";
+            buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+            buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+
+        [MenuItem("Game Jam/Build Tools/Linux/Server")]
+        public static void BuildLinuxServer()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/Server.unity", "Assets/Scenes/Game.unity" };
+            buildPlayerOptions.locationPathName = "Builds/Server/Windows/server.X86_64";
+            buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
+            buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+
+        [MenuItem("Game Jam/Build Tools/Web GL/Client")]
+        public static void BuildWebClient()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/Client.unity", "Assets/Scenes/Game.unity" };
+            buildPlayerOptions.locationPathName = "Builds/Client/Windows/client.exe";
+            buildPlayerOptions.target = BuildTarget.WebGL;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+    }
+}
